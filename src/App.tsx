1b2d3d255a880d3c297cc8fb0d99aa11323d4d23
Jeff B <105983864+Jeff-Shango@users.noreply.github.com>
2023-03-05 22:20:31 -0500

    creating calendar widget
    want to add more functionality later

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import NavLinks from './components/NavbarFolder/NavLinks';
 import Text from './components/TextFolder/Text';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './app.css'
+import {CalendarComponent} from '@syncfusion/ej2-react-calendars'
 
 const App = () => {
+  const dateValue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 13);
+  const startDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 6);
+  const endDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 25);
   return (
     <div id='container'>
       <NavLinks/>
       <Background/>
       <Text/>
+      <CalendarComponent id='calendarWid' value={dateValue} min={startDate} max={endDate}></CalendarComponent>
     </div>
   )
 }
